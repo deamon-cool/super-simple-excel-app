1e07efdb708eca8d8a042d45d91a617e329f6fab
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-08-17 13:24:10 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,118 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const numRegex = /^\d+$/;
+
 function App() {
+  const [matrixDimension, setMatrixDimension] = useState(0);
+  const [values, setValues] = useState<Record<string, string>>({});
+
+  useEffect(() => {
+    for (let i = 0; i < matrixDimension; i++) {
+      for (let j = 0; j < matrixDimension; j++) {
+        const tdId = `${i}x${j}`;
+        const value = Math.floor(Math.random() * 1000).toString();
+
+        setValues(prevVal => ({
+          ...prevVal,
+          [tdId]: value
+        }));
+      }
+    }
+  }, [matrixDimension]);
+
+  const increaseMatrixHandler = () => {
+    setMatrixDimension(prevMatrixDim => prevMatrixDim + 1);
+  };
+
+  const decreaseMatrixHandler = () => {
+    if (matrixDimension > 0) {
+      setMatrixDimension(prevMatrixDim => prevMatrixDim - 1);
+    }
+  };
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>, tdId: string) => {
+    const value = e.target.value;
+
+    if (value.length > 3) return;
+
+    if (numRegex.test(value) || value.length === 0) {
+      setValues(preVal => {
+        return {
+          ...preVal,
+          [tdId]: e.target.value
+        }
+      });
+    }
+  };
+
+  let tableBody: JSX.Element[] = [];
+  let totalSum = 0;
+
+  for (let i = 0; i < matrixDimension; i++) {
+    let tds: JSX.Element[] = [];
+
+    let sum = 0;
+
+    for (let j = 0; j < matrixDimension; j++) {
+      const tdId = `${i}x${j}`;
+
+      sum += Number(values[tdId] ? values[tdId] : 0);
+
+      tds.push(
+        <td key={tdId}>
+          <input
+            value={values[tdId] ? values[tdId] : ''}
+            onChange={(e) => onChangeHandler(e, tdId)} />
+        </td>
+      );
+    }
+
+    totalSum += sum;
+
+    tds.push(
+      <td key={`td-${i}`}>
+        <div className='rowSum'>
+          {sum}
+        </div>
+      </td>
+    );
+
+    const tr = <tr key={`tr-${i}`}>{tds}</tr>;
+    tableBody.push(tr);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Super Simple Excel</h1>
+      <div className='info'>Click button + or - to increase or decrease matrix</div>
+      <div className='buttons'>
+        <button onClick={increaseMatrixHandler}>
+          +
+        </button>
+        <button onClick={decreaseMatrixHandler}>
+          -
+        </button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th colSpan={matrixDimension}>Matrix n x n</th>
+            <th>Sum</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableBody}
+          <tr>
+            <td colSpan={matrixDimension}></td>
+            <td>
+              <div className='totalSum'>
+                {totalSum}
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
